c47167c5f1e6c55467cb46019aac0304c28b9e3e
Mardiprk <mk1659770@gmail.com>
2025-07-03 08:20:29 +0000

    token vesting

---
+++ programs/token-vesting/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("2mxPnkEdXXVVevKofXooFKGYx8eVqJ6wEWNZz6AY8GWR");
+
+#[program]
+pub mod token_vesting {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
